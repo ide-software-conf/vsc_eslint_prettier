2c067a0f7988cd0fa144a4ae8528bb9c078f402e
bi-kai <bikai_xidian@163.com>
2022-07-23 23:53:54 +0800

    调整配置文件路径

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,18 @@
 module.exports = {
-  root: true,
+  root: true, // 代表顶层配置，不要再往父级查找配置文件
+  // 指定环境的全局变量
   env: {
+    browser: true,
     commonjs: true,
     es6: true,
     node: true
   },
   parser: 'babel-eslint',
   // extends第一项与工程强相关，比如vue、react都有专门的规则模块
-  extends: ['plugin:prettier/recommended'],
+  extends: [
+    'eslint-config-egg', // eggjs
+    'plugin:prettier/recommended'
+  ],
   parserOptions: {
     // parser: 'babel-eslint',
     ecmaVersion: 2015
